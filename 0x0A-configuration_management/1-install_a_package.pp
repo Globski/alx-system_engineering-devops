1c4f7be8fe511aacf2eb5ebc915eb4d95d2c67ec
Gloriana <141042662+Globski@users.noreply.github.com>
2024-07-03 09:41:38 -0700

    Install Werkzeug 2.1.1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Install Flask version 2.1.0 using pip3.
+# Install Flask version 2.1.0 and Werkzeug version 2.1.1 using pip3.
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3', 
 }
