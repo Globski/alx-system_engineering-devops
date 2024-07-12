802acb33e694b28774c736cf9f62c505cd247fff
Gloriana <141042662+Globski@users.noreply.github.com>
2024-07-11 19:07:42 -0700

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,4 @@
 # Puppet manifest to configure SSH client
-file { '/etc/ssh/ssh_config':
-  ensure  => file,
-  content => template('ssh/ssh_config.erb'),
-}
-
 file_line { 'Turn off password authentication':
   path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
@@ -14,28 +9,4 @@ file_line { 'Use the private key':
   path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
   match => '^#IdentityFile',
-}
-
-file { '/home/ubuntu/.ssh/config':
-  ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
-  content => template('ssh/ssh_config.erb'),
-}
-
-file { '/home/ubuntu/.ssh/school':
-  ensure => 'present',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0600',
-  source => 'puppet:///modules/ssh/school',
-}
-
-file { '/home/ubuntu/.ssh/school.pub':
-  ensure => 'present',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0644',
-  source => 'puppet:///modules/ssh/school.pub',
 }
