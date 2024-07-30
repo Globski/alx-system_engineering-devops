9b651ffbd8d4d4d43e3356b94b8f0f6766dabcd6
Gloriana <141042662+Globski@users.noreply.github.com>
2024-07-30 04:01:01 -0700

    Export employee's to-do list to CSV.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,6 +4,7 @@ import csv
 import requests
 import sys
 
+
 def fetch_and_export_todo_csv(employee_id):
     """
     Retrieve and export to-do list for an employee to a CSV file.
@@ -14,7 +15,8 @@ def fetch_and_export_todo_csv(employee_id):
     user_response = requests.get(f"{base_url}/users/{employee_id}")
     user_data = user_response.json()
 
-    todo_response = requests.get(f"{base_url}/todos", params={"userId": employee_id})
+    todo_url = f"{base_url}/todos", params={"userId": employee_id}
+    todo_response = requests.get(todo_url)
     todo_list = todo_response.json()
 
     file_name = f"{employee_id}.csv"
@@ -22,6 +24,7 @@ def fetch_and_export_todo_csv(employee_id):
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in todo_list:
             writer.writerow([employee_id, user_data.get("username"), task.get("completed"), task.get("title")])
+
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
