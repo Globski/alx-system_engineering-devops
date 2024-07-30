2454596a863e85322bba4b10972d529fc9db85cb
Gloriana <141042662+Globski@users.noreply.github.com>
2024-07-30 04:09:51 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -33,9 +33,5 @@ def fetch_employee_todo_progress(employee_id):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
     employee_id = int(sys.argv[1])
     fetch_employee_todo_progress(employee_id)
