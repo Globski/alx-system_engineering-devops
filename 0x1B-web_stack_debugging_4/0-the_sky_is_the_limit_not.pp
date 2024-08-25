20f554b17765966a23637958192b261627d9f487
globski <gloriaogunsemore@gmail.com>
2024-08-25 17:15:12 +0000

    Increase Nginx worker connections to handle more simultaneous connections.

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,21 @@
+# Increases the amount of traffic an Nginx server can handle.
+# Increase the ULIMIT of the default file from 15 to 4096
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'restart-nginx':
+  command => '/usr/sbin/service nginx restart'
+}# Increases the amount of traffic an Nginx server can handle.
+# Increase the ULIMIT of the default file from 15 to 4096
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'restart-nginx':
+  command => '/usr/sbin/service nginx restart'
+}
